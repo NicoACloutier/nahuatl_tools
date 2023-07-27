3f54cd311448d7268f9ecd9f3374d067e2251d42
Nicolas Antonio Cloutier Arce <nicocloutier1@gmail.com>
2023-07-27 17:38:36 -0400

    add additional steps to `is_verb` algorithm

--- src/pos_tagger/pos_tagger.py
+++ src/pos_tagger/pos_tagger.py
@@ -1,7 +1,7 @@
-import sys
+import sys, typing
 from .. import parse
 
-def is_verb_rb(word: str, verb_list: list[str], non_verb_list: list[str]) -> bool:
+def is_verb_rb(word: str, verb_list: list[str], non_verb_list: list[str]) -> typing.Optional[bool]:
 	'''
 	Rules-based algorithm to determine whether a Nahuatl word is a verb.
 	Arguments:
@@ -13,6 +13,7 @@ def is_verb_rb(word: str, verb_list: list[str], non_verb_list: list[str]) -> boo
 	'''
 	noun_morphemes, noun_lemma = parse.parse_noun(word)
 	verb_morphemes, verb_lemma = parse.parse_verb(word)
+	noun_lemma_pos, verb_lemma_pos = noun_morphemes.index(noun_lemma), verb_morphemes.index(verb_lemma)
 	_, absolutive = parse.search_absolutive(word)
 
 	#check for presence in the wordlists
@@ -36,8 +37,21 @@ def is_verb_rb(word: str, verb_list: list[str], non_verb_list: list[str]) -> boo
 	if verb_lemma.endswith('oa') or verb_lemma.endswith('ia') or verb_morphemes[0] == 'xi':
 		return True
 
+	#check for tense and directional suffixes
 	for suffix in parse.TENSE_SUFFIXES_V + parse.DIRECTIONAL_SUFFIXES_V:
-		if suffix in verb_morphemes:
+		if suffix in verb_morphemes[verb_lemma_pos:] and suffix != 'ko':
 			return True
 
-	return False
+	#check for object prefixes
+	for prefix in parse.OBJECT_PREFIXES_V:
+		if prefix in verb_morphemes[:verb_lemma_pos]:
+			return True
+
+	#check for both a genitive prefix and suffix (e.g. <yo>, <wa(n)>)
+	if any(prefix in noun_morphemes[:noun_lemma_pos] for prefix in parse.GENITIVE_PREFIXES_N):
+		return False
+
+	if any(suffix in noun_morphemes[noun_lemma_pos:] for suffix in parse.DIMINUTIVE_SUFFIXES_N):
+		return False
+
+	return None
